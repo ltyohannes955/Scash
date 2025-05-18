3bd308b72f6f657a8f9db3a8f43063520838000f
ltyohannes955 <ltyohannes95@gmail.com>
2025-05-18 21:54:32 +0300

    basic set up sign up

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,7 +1,47 @@
+import {
+  Flex,
+  Text,
+  Image,
+  Input,
+  PasswordInput,
+  ActionIcon,
+} from "@mantine/core";
+import { Airplay } from "lucide-react";
 export default function Signup() {
   return (
     <>
-      <h1>sign up</h1>
+      <Flex
+        bg={"black"}
+        h={"100vh"}
+        justify={"space-evenly"}
+        align={"center"}
+        direction={"column"}
+      >
+        <Flex direction={"column"}>
+          <Image
+            src={"/password.svg"}
+            alt="landing Image"
+            h={"500"}
+            w={"400"}
+          />
+          <Text c="white" fz={"h1"} ta={"center"} fw={"bolder"}>
+            Login
+          </Text>
+        </Flex>
+        <Flex direction={"column"} w={"100%"} align={"center"}>
+          <Input placeholder="Enter Your email" w={"65%"} />
+          <PasswordInput
+            w={"65%"}
+            label="Password"
+            placeholder="Enter Your password"
+          />
+          <Flex>
+            <ActionIcon variant="subtle" aria-label="Settings">
+              <Airplay />
+            </ActionIcon>
+          </Flex>
+        </Flex>
+      </Flex>
     </>
   );
 }
