ebc6038e87a266d40497b3bc00dbc8b580f24ebd
ltyohannes955 <ltyohannes95@gmail.com>
2025-05-29 21:20:10 +0300

    removed uneeded code

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -34,7 +34,6 @@ export default function Signup() {
             label="Password"
             placeholder="Enter Your password"
           />
-          <Flex gap={"lg"} mt={"lg"}></Flex>
         </Flex>
         <Button w={"85%"}>Sign up</Button>
       </Flex>
