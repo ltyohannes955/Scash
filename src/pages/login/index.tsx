ebeb4cd775a2b36d3c132022eefc2aab9c15c58f
ltyohannes955 <ltyohannes95@gmail.com>
2025-05-30 22:19:35 +0300

    typo fix

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -56,7 +56,7 @@ export default function Login() {
                 navigate("/signup");
               }}
             >
-              Sign Up
+              SignUp
             </Text>
           </Text>
         </Flex>
