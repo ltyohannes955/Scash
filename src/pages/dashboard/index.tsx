d68061fa7c4f194877c417d800245ff73288aa0b
ltyohannes955 <ltyohannes95@gmail.com>
2025-06-03 21:49:55 +0300

    added the components to the main page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,18 @@
-import { Flex } from "@mantine/core";
+import { Flex, Text } from "@mantine/core";
 import { RootShell } from "../../components/layout/shell/root";
+import { Cards } from "./card";
+import { AddButton } from "./addButton";
 
 export default function Dashboard() {
   return (
     <>
       <RootShell Title="Dashboard">
+        <Text size="md" c={"white"} fw={"bold"}>
+          Account
+        </Text>
         <Flex bg={"#122117"} h={"80vh"} align={"center"} direction={"column"}>
-          <Flex>//card</Flex>
-          <Flex>//transaction</Flex>
+          <Cards />
+          <AddButton />
         </Flex>
       </RootShell>
     </>
