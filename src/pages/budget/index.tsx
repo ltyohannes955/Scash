a6ba490673a1514ccf36abb681a517f460489227
ltyohannes955 <ltyohannes95@gmail.com>
2025-06-13 22:49:20 +0300

    base set up for the budget page

---
+++ src/pages/budget/index.tsx
@@ -0,0 +1,14 @@
+import { Flex, Text } from "@mantine/core";
+import { RootShell } from "../../components/layout/shell/root";
+
+export default function Budget() {
+  return (
+    <>
+      <RootShell Title="Budget">
+        <Flex>
+          <Text> Budget</Text>
+        </Flex>
+      </RootShell>
+    </>
+  );
+}
